fa0cef835f57084e1a1efdc4a949b707a170833f
douglasans <douglasans@hotmail.com>
2021-10-25 22:56:38 -0300

    inicio animação

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/CardComponent.module.css';
 
-export default function CardComponent() {
+export default class CardComponent extends React.Component {
+       
 
-    var description = "Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50"
-    var oldPrice = "R$ 2.813,99"
-    var price = "R$ 2.599,00"
-    var installments = "em até 10x de R$ 259,90 sem juros"
+    render() {
+        var description = "Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50"
+        var oldPrice = "R$ 2.813,99"
+        var price = "R$ 2.599,00"
+        var installments = "em até 10x de R$ 259,90 sem juros"
 
+        var  abox = document.getElementById(styles.aaa)
+        
+        console.log(abox)
+        function addProduct(){
+            console.log(abox)
+            //abox.classList.toggle("add-favorites")
+        }
+    
 
     return (
         <div className={styles.container}>
-            <div className={styles.imgProduto}>
-            <img src="./Ellipse1.png" alt="Ellipse" />
-            <img src="./Vector1.png" alt="Vector"/>
+            <div className={styles.imgProduto} >
+                <div id={styles.aaa} ></div>
+                <div ></div>
             </div>
             <h2 className={styles.description}>{description}</h2>
             <h2 className={styles.oldPrice}>{oldPrice}</h2>
             <h2 className={styles.price}>{price}</h2>
             <h2 className={styles.installments}>{installments}</h2>
-            <button className={styles.btnAdicionar} >adicionar</button>
-            
+            <button className={styles.btnAdicionar} onClick={addProduct} >adicionar</button>
+           
         </div>
     )
+    }
 }
