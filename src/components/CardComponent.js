a72e69fd43844f470bf3ff91b85e6f32b8b7cbd8
douglasans <douglasans@hotmail.com>
2021-10-26 00:46:58 -0300

    animação favoritos em andamento

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,37 +1,43 @@
 import React, { useState } from 'react'
 import styles from '../styles/CardComponent.module.css';
 
-export default class CardComponent extends React.Component {
+export default function CardComponent() {
        
 
-    render() {
+    
         var description = "Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50"
         var oldPrice = "R$ 2.813,99"
         var price = "R$ 2.599,00"
         var installments = "em até 10x de R$ 259,90 sem juros"
 
-        var  abox = document.getElementById(styles.aaa)
-        
-        console.log(abox)
         function addProduct(){
-            console.log(abox)
-            //abox.classList.toggle("add-favorites")
+        }
+
+        function addFavorite(){
+            var checkbox = document.getElementById(styles.favCheckbox)
+            checkbox.checked = true
         }
     
 
     return (
         <div className={styles.container}>
+            <input type="checkbox" id={styles.favCheckbox}/>
             <div className={styles.imgProduto} >
-                <div id={styles.aaa} ></div>
-                <div ></div>
+                
+                <div onClick={addFavorite} className={styles.a}></div>
+                <div onClick={addFavorite} className={styles.b}></div>
+                
+                
             </div>
             <h2 className={styles.description}>{description}</h2>
             <h2 className={styles.oldPrice}>{oldPrice}</h2>
             <h2 className={styles.price}>{price}</h2>
             <h2 className={styles.installments}>{installments}</h2>
             <button className={styles.btnAdicionar} onClick={addProduct} >adicionar</button>
-           
+            
+            
+            
         </div>
     )
-    }
+    
 }
